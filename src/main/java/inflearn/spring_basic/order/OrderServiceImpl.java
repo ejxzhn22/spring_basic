cde38140f66ef3d5347111a30c92a144941fe497
sujin <nijus299@gmail.com>
2022-03-29 19:46:18 +0900

    새로운 할인 정책 적용과 문제점

--- src/main/java/inflearn/spring_basic/order/OrderServiceImpl.java
+++ src/main/java/inflearn/spring_basic/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package inflearn.spring_basic.order;
 
 import inflearn.spring_basic.discount.DiscountPolicy;
 import inflearn.spring_basic.discount.FixDiscountPolicy;
+import inflearn.spring_basic.discount.RateDiscountPolicy;
 import inflearn.spring_basic.member.Member;
 import inflearn.spring_basic.member.MemberRepository;
 import inflearn.spring_basic.member.MemoryMemberRepository;
@@ -9,7 +10,11 @@ import inflearn.spring_basic.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+  //  private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+  //  private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
